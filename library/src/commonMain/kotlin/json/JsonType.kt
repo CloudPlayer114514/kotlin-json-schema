75e559eb65bb63bbdd7d14278b66bfd1cf76f957
CloudPlayer <2909078582@qq.com>
2024-08-04 23:06:16 +0800

    增加 jvm 的 actual。

--- library/src/commonMain/kotlin/json/JsonType.kt
+++ library/src/commonMain/kotlin/json/JsonType.kt
@@ -1,16 +1,52 @@
 package com.yunbao.kotlin.schema.json
 
+import kotlinx.serialization.json.*
+
 /**
  * 在将字符串解析为 Json 对象时提供的建议。解析时将尝试将字符串中的内容与提供的类型相匹配，如果匹配失败，则将其解析为 JsonElement 。
  *
  * @author CloudPlayer
  */
 enum class JsonType {
-    Null,
-    String,
-    Number,
-    Boolean,
-    Array,
-    Object,
-    JsonElement,
+    NULL {
+        override fun toJsonElement(value: String): JsonNull {
+            return JsonNull
+        }
+    },
+    STRING {
+        override fun toJsonElement(value: String): JsonPrimitive {
+            return JsonPrimitive(value)
+        }
+    },
+    NUMBER {
+        override fun toJsonElement(value: String): JsonPrimitive {
+            return JsonPrimitive(value.toDoubleOrNull() ?: value.toLong())
+        }
+    },
+    BOOLEAN {
+        override fun toJsonElement(value: String): JsonPrimitive {
+            return JsonPrimitive(value.toBooleanStrict())
+        }
+    },
+    ARRAY {
+        override fun toJsonElement(value: String): JsonArray {
+            return buildJsonArray {
+                Json.decodeFromString<Array<out JsonElement>>(value).forEach {
+                    add(it)
+                }
+            }
+        }
+    },
+    OBJECT {
+        override fun toJsonElement(value: String): JsonElement {
+            return Json.parseToJsonElement(value)
+        }
+    },
+    JSON_ELEMENT {
+        override fun toJsonElement(value: String): JsonElement {
+            return Json.parseToJsonElement(value)
+        }
+    };
+
+    abstract fun toJsonElement(value: String): JsonElement
 }
